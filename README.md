86d50d8d433fc71d81c937a2a95818b6052c9521
Tim Beyer <tim.beyer@gmail.com>
2014-02-07 15:36:41 +0100

    Shim for bluebird + redis

---
+++ README.md
@@ -0,0 +1,6 @@
+# continuation-local-storage support for bluebird promises
+
+### Quick and dirty port from https://github.com/othiym23/cls-q
+
+For documentation, check the above link.
+It works in the same way and uses the same tests.
---
+++ shim.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var shimmer = require('shimmer');
+
+module.exports = function patchBluebird(ns) {
+    if (typeof ns.bind !== 'function') {
+        throw new TypeError("must include namespace to patch bluebird against");
+    }
+
+    var Promise = require('bluebird');
+    var proto = Promise && Promise.prototype;
+    shimmer.wrap(proto, '_addCallbacks', function(_addCallbacks) {
+        return function ns_addCallbacks(fulfill, reject, progress, promise, receiver) {
+            if (typeof fulfill === 'function') fulfill = ns.bind(fulfill);
+            if (typeof reject === 'function') reject = ns.bind(reject);
+            if (typeof progress === 'function') progress = ns.bind(progress);
+
+            return _addCallbacks.call(this, fulfill, reject, progress, promise, receiver);
+        };
+    });
+};
